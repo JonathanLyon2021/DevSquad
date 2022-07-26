52e49000aab59675a601a5f6921e773197d74a2c
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-25 23:42:14 -0400

    Create Imo.sol
    Added contract me.

---
+++ Imo.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+    contract Me {
+            address public senderOne;
+            address public senderTwo;
+            address public senderThree;
+    }
+        
