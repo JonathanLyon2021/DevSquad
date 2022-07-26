729fe04c1492c3a77f31368244c860dac9891572
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-25 23:45:10 -0400

    Update Imo.sol
    Added interface Imo.

--- Imo.sol
+++ Imo.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+interface Imo{
+    function callMeDifferentContract()external returns(address);
+}
     contract Me {
             address public senderOne;
             address public senderTwo;
