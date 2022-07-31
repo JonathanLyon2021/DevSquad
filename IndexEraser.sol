422d57840cd5f8f08a577b925226f86036df80df
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-31 17:12:01 -0400

    Update IndexEraser.sol
    Added a public method/function removeIndex() which takes a parameter(index) and returns an array(_arr) which is stored in memory.

--- IndexEraser.sol
+++ IndexEraser.sol
@@ -5,6 +5,14 @@ contract IndexEraser{
 
     uint[] private arr1 = [0,1,2,3,4,5,6,7,8];
 
+function removeIndex(uint index)public returns(uint[] memory _arr){
+        require(index < arr1.length -1, "INDEX OUT OF BOUNDS!!");
+        _arr = arr1;
+        _arr[index] = _arr[_arr.length -1];
+        arr1 = _arr;
+        arr1.pop(); 
+    }
+
     function viewArray()public view returns(uint[] memory){
         return arr1;
     }
